16a8086bcf1618e6a032263e1dd66430b38184c1
Kobi Meirson <kobi@right.co.il>
2018-10-04 14:32:10 +0300

    On-the-fly queue additions

--- client/js/queuesListController.js
+++ client/js/queuesListController.js
@@ -1,15 +1,19 @@
 module.exports = QueuesListController;
 
 QueuesListController.$inject = ['queues', '$cookies'];
-function QueuesListController (queues, $cookies) {
-    var vm = this;
 
-    vm.active = $cookies.get('selectedQueue') || null;
-    vm.queues = [];
-    vm.select = select;
+function hasQueue(queues, name) {
+    return queues.filter(function (q) {
+        return q.name === name;
+    }).length > 0;
+}
 
+function loadQueues(queues, vm) {
     queues.getQueues().then(function (queues) {
         queues.forEach(function (q) {
+            if (hasQueue(vm.queues, q)) {
+                return;
+            }
             if (!vm.active) {
                 vm.active = q;
             }
@@ -20,6 +24,19 @@ function QueuesListController (queues, $cookies) {
             });
         });
     });
+}
+
+var queuesInterval;
+
+function QueuesListController (queues, $cookies) {
+    var vm = this;
+
+    vm.active = $cookies.get('selectedQueue') || null;
+    vm.queues = [];
+    vm.select = select;
+
+    loadQueues(queues, vm);
+    queuesInterval = setInterval(loadQueues.bind(null, queues, vm), 20000);
 
     function select (queue) {
         $cookies.put('selectedQueue', queue);
